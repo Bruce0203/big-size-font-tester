20e74a551a64951de0913b45204cd5f86164f092
Bruce0203 <56539682+Bruce0203@users.noreply.github.com>
2022-10-30 06:54:27 +0000

    init

---
+++ myapp/src/App.svelte
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+
+<strong id="text">text</strong>
+<form>
+  <input type="text" id="text" name="text" placeholder="text" required>
+</form>
+<style>
+  form {
+    position: absolute;
+  bottom: 0px;
+  }
+</style>
+<script>
+const input = document.querySelector('input');
+const log = document.getElementById('text');
+
+input.addEventListener('input', updateValue);
+
+function updateValue(e) {
+  log.textContent = e.target.value;
+}
+
+</script>
